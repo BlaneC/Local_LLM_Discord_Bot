607a556b4017393e217fc2d155f3b7522705bd3e
Blane Cypurda <bcypurda286@gmail.com>
2023-12-23 14:05:31 -0600

    working

--- bot.py
+++ bot.py
@@ -3,14 +3,20 @@ from discord.ext import commands
 import json
 import aiohttp
 import asyncio
+import aiofiles
 import os
 from dotenv import load_dotenv
 import subprocess
+import re
+import base64
+
 
 # Load the environment variables from .env file
 load_dotenv()
 
 model = "dolphin-mixtral:8x7b-v2.5-q4_0"  # Update this for whatever model you wish to use
+llava_model = "llava:13b"
+
 BOT_NAME = "Dolphin Mixtral Bot"
 STREAM_UPDATE_PERIOD = 0.5  # Time in seconds between message updates
 MAX_MESSAGE_LENGTH = 1500  # Maximum Discord message length before sending a new message
@@ -20,6 +26,9 @@ SYSTEM_MESSAGE = """I am a helpful Discord bot, here to answer your questions, p
                 I am programmed to be patient and understanding, so feel free to ask me anything, no matter how simple or complex the question may be. 
                 My primary goal is to ensure that our interaction is informative, engaging, and enjoyable for all parties involved. Let's work together to broaden our knowledge and perspectives!
                 """
+TEMP_IMAGE_DIR = r"C:\Users\bcypu\Documents\Projects\Discord-Bot\temp"
+DOCKER_IMAGE_DIR = "/tmp/temp_images"   # Path inside the Docker container
+
 intents = discord.Intents.default()
 intents.messages = True
 intents.reactions = True
@@ -31,16 +40,28 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 contexts = {}
 content_lock = asyncio.Lock()
 
-
 def start_docker_container():
     try:
-        # Run the Docker container
-        try:
-            subprocess.run(["docker", "run", "-d", "--gpus=all", "-v", "ollama:/root/.ollama", "-p", "11434:11434", "--name", "ollama", "ollama/ollama"], check=True)
-        except subprocess.CalledProcessError as e:
-            print(f"Error running docker container: {e}")
+        # Create the temporary image directory if it doesn't exist
+        os.makedirs(TEMP_IMAGE_DIR, exist_ok=True)
+
+        subprocess.run([
+            "docker", "run", "-d", "--gpus=all",
+            "-v", "ollama:/root/.ollama",
+            "-p", "11434:11434", "--name", "ollama", "ollama/ollama"
+        ], check=True)
+        # Run the Docker container with the temporary image directory volume
+        subprocess.run([
+            "docker", "run", "-d", "--gpus=all",
+            "-e", "OLLAMA_HOST=0.0.0.0:11435"
+            "-v", "ollama:/root/.ollama",
+            "-p", "11435:11435", "--name", "ollama_lava", "ollama/ollama"
+        ], check=True)
+
         # Execute the program in the container (non-interactive mode)
         subprocess.run(["docker", "exec", "ollama", "ollama", "run", model], check=True)
+        subprocess.run(["docker", "exec", "ollama_lava", "ollama", "run", "llava:13b"], check=True)
+
         print("Docker container started and program executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while starting the Docker container: {e}")
@@ -68,8 +89,6 @@ async def update_message_periodically(bot_message, shared_content):
             elif current_content:
                 # Edit the existing message with the current content
                 await bot_message.edit(content=current_content)
-
-
 
 async def stream_chat(messages, shared_content):
     message_buff = ""
@@ -115,6 +134,53 @@ async def clear_context(ctx):
     else:
         await ctx.send("No context to clear.")
 
+async def download_image(url, session, download_path):
+    try:
+        async with session.get(url) as response:
+            if response.status == 200:
+                # Extract the filename and sanitize it
+                filename = re.sub(r'[\\/*?:"<>|]', "", os.path.basename(url).split('?')[0])
+                filepath = os.path.join(download_path, filename)
+                
+                async with aiofiles.open(filepath, 'wb') as file:
+                    await file.write(await response.read())
+                return filepath
+    except Exception as e:
+        print(f"Error downloading image: {e}")
+    return None
+
+async def get_image_descriptions(images):
+    descriptions = []
+    os.makedirs(TEMP_IMAGE_DIR, exist_ok=True)
+
+    async with aiohttp.ClientSession() as session:
+        for image_url in images:
+            image_path = await download_image(image_url, session, TEMP_IMAGE_DIR)
+            if image_path:
+                # Read the image and encode it to base64
+                async with aiofiles.open(image_path, 'rb') as image_file:
+                    image_data = await image_file.read()
+                    base64_encoded_image = base64.b64encode(image_data).decode('utf-8')
+
+                payload = {
+                    "model": llava_model,
+                    "prompt": "Describe in detail what is in this picture. Preface your description with a title for the picture. Write all text that appears in the image seperately from your main description, word for word",
+                    "stream": False,
+                    "images": [base64_encoded_image]
+                }
+
+                async with session.post("http://localhost:11435/api/generate", json=payload) as response:
+                    if response.status == 200:
+                        result = await response.json()
+                        description = result.get("response", "")
+                        if description:
+                            descriptions.append(description)
+                
+                # Delete the image from the host after processing
+                os.remove(image_path)
+
+    return descriptions
+
 @bot.event
 async def on_message(message):
     if message.channel.name != 'llm':
@@ -129,9 +195,24 @@ async def on_message(message):
 
     guild_id = message.guild.id
     create_context(guild_id)
-    contexts[guild_id].append({"role": "user", "content": message.content})
 
+    image_descriptions = []
     bot_message = await message.channel.send("Thinking...")
+
+    if message.attachments and message.channel.permissions_for(message.guild.me).attach_files:
+        await bot_message.edit(content="Looking at images...")
+        # Filter attachments for image file types
+        images_to_process = [attachment.url for attachment in message.attachments if any(attachment.filename.lower().endswith(ext) for ext in ('.png', '.jpg', '.jpeg', '.svg'))]
+        image_descriptions = await get_image_descriptions(images_to_process)
+
+    elif not message.channel.permissions_for(message.guild.me).attach_files:
+        await message.channel.send("I don't have permission to analyze images.")
+
+    async with content_lock:
+        image_description_text = '\nDescription of images attached: \n'.join(image_descriptions)
+        contexts[guild_id].append({"role": "user", "content": message.content + image_description_text})
+
+    await bot_message.edit(content="Thinking...")
     shared_content = {'content': '', 'done': False}
 
     # Run streaming and message updating concurrently
@@ -141,6 +222,7 @@ async def on_message(message):
     )
     async with content_lock:
         contexts[guild_id].append({"role": "assistant", "content": shared_content["content"]})
+
 
 
 # Call the function to start the Docker container
